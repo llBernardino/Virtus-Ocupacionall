bcf02dda10576dc0e665ebf67015c0176733f01c
Caua <caua.lima@advance.com.br>
2024-05-20 16:15:00 -0300

    Feat: project creation and page organization/ First Session (Hero)

---
+++ src/components/Hero/style.ts
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled'
+
+const background = () => `url('${'paper'}')`
+export const Content = styled.div`
+  
+`
+
+export const BackgroundHero = styled.div`
+    min-height:300px;
+    display: flex;
+    background-image: linear-gradient(90deg,  #64CBE8 0%,  #64CEB8 100%)
+`
+export const Title = styled.h1`
+    color: #FFFFFF;
+    font-family: "Roboto", Sans-serif;
+    font-size: 40px;
+    font-weight: 600;
+    text-transform: none;
+    stroke: #000;
+    max-width: 535px;
+`
+export const TextHero = styled.p`
+    color: #FFFFFF;
+    font-family: "Roboto", Sans-serif;
+    font-size: 25px;
+    font-weight: 400;
+`
+export const Image = styled.img`
+
+`
+export const Boxtext = styled.div`
+
+`
+
---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+
+export const AppRoutes = () => {
+  
+
+  return (
+    <Routes>
+      <Route path="/pagina-inicial"  />
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
+  );
+};
