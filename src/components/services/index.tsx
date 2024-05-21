b4b1b932821a7383f85c640251ce05299709986b
Caua <caua.lima@advance.com.br>
2024-05-21 12:39:18 -0300

    Feat:Development of the fourth session completed / and the new version of the card (Services)

--- src/components/services/index.tsx
+++ src/components/services/index.tsx
@@ -1,10 +1,11 @@
-import Servicesimg from '../../imgs/sobreimg.png';
-import banco from '../../imgs/banco.png';
-import digital from '../../imgs/digital.png';
-import gestao from '../../imgs/gestao.png';
-import onboard from '../../imgs/onboard.png';
-import saude from '../../imgs/saude.png';
-import search from '../../imgs/search.png';
+import medic from '../../imgs/medic.svg';
+import signa from '../../imgs/signa.svg';
+import clipboard from '../../imgs/clipboard.svg';
+import book from '../../imgs/book.svg';
+import desktop from '../../imgs/desktop.svg';
+import usershield from '../../imgs/usershield.svg';
+import street from '../../imgs/street.svg';
+import wpforms from '../../imgs/wpforms.svg';
 import {
     Content,BackgroundServices, 
     Title, 
@@ -26,29 +27,29 @@ import { CardG } from './comp/card';
             </Box>
             <Grid container spacing={2} textAlign='center' justifyContent='center' alignItems='start'>
               <Grid sm={12} md={5} lg={3} display='flex' alignItems='ceter' p={2} flexDirection = 'column' justifyContent='center' >
-                <CardG image = {search} titleG = {'HUNTING & SEARCH'} textG = {'Encontrar, recrutar e selecionar talentos'}/>
+                <CardG image = {street} titleG = {'CIPA ONLINE'} textG = {'Treinamento de CIPA 100% online, com um sistema especializado para tornar essa experiência única.'}/>
               </Grid>
               <Grid sm={12} md={5} lg={3} display='flex' alignItems='ceter' p={2} flexDirection = 'column' justifyContent='center'> 
-                <CardG image = {saude} titleG = {'SAÚDE E SEGURANÇA OCUPACIONAL'} textG = {'Exames, laudos, treinamentos presenciais e EAD'}/>
+                <CardG image = {usershield} titleG = {'PGR E PCMSO'} textG = {'Medidas que evitam acidentes de trabalho e diminuem os riscos de doenças ocupacionais.'}/>
               </Grid>
               <Grid sm={12} md={5} lg={3} display='flex' alignItems='ceter' p={2} flexDirection = 'column' justifyContent='center' >
-                <CardG image = {digital} titleG = {'DIGITAL BOARDING'} textG = {'Análise e gestão de documentação admissional'}/>
+                <CardG image = {clipboard} titleG = {'E-SOCIAL'} textG = {'Validamos os documentos e legalidade junto ao E-social de forma automática.'}/>
               </Grid>
               <Grid sm={12} md={5} lg={3} display='flex' alignItems='ceter' p={2} flexDirection = 'column' justifyContent='center' >
-                <CardG image = {gestao} titleG = {'GESTÃO DE BENEFÍCIOS'} textG = {'Clube de benefícios, saúde, dental, vida e previdência'}/>
+                <CardG image = {book} titleG = {'TREINAMENTOS PRESENCIAIS E EAD'} textG = {'Primeiros Socorros , EPI’s, Trabalho em Altura, NR10, Reciclagem de Empilhadeira, entre outros.'}/>
               </Grid>
 
               <Grid sm={12} md={5} lg={3} display='flex' alignItems='ceter' p={2} flexDirection = 'column' justifyContent='center'> 
-                <CardG image = {onboard} titleG = {'ONBOARDING'} textG = {'Integração e desenvolvimento contínuo'}/>
+                <CardG image = {desktop} titleG = {'AUTO AGENDAMENTO'} textG = {'Pode ser feito diretamente pelo colaborador, pela empresa…'}/>
               </Grid>
               <Grid sm={12} md={5} lg={3} display='flex' alignItems='ceter' p={2} flexDirection = 'column' justifyContent='center' >
-                <CardG image = {banco} titleG = {'BANCO DE TALENTOS'} textG = {'Elaboração, implantação e políticas'}/>
+                <CardG image = {signa} titleG = {'GESTÃO OCUPACIONAL E DE EXAMES'} textG = {'Atua na diminuição da margem de erro e redução de custos.'}/>
               </Grid>
               <Grid sm={12} md={5} lg={3} display='flex' alignItems='ceter' p={2} flexDirection = 'column' justifyContent='center'> 
-                <CardG image = {onboard} titleG = {'ONBOARDING'} textG = {'Integração e desenvolvimento contínuo'}/>
+                <CardG image = {medic} titleG = {'EXAMES MÉDICOS (ASO DIGITAL)'} textG = {'O médico assina digitalmente o ASO e em seguida sobe para a nossa plataforma.'}/>
               </Grid>
               <Grid sm={12} md={5} lg={3} display='flex' alignItems='ceter' p={2} flexDirection = 'column' justifyContent='center' >
-                <CardG image = {banco} titleG = {'BANCO DE TALENTOS'} textG = {'Elaboração, implantação e políticas'}/>
+                <CardG image = {wpforms} titleG = {'APLICAÇÃO DE TODAS AS NRs' } textG = {'A aplicação das NRs promove a integridade, o bem-estar e a satisfação dos trabalhadores.'}/>
               </Grid>
             </Grid>
           </Container>
